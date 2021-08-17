cea518300017413980fa79b4d505273b347b9de6
David Gasana Manzi <gmdavid59@gmail.com>
2021-08-17 18:11:40 +0200

    fix incorrect server public ip

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 
-server '100.24.238.5', user: 'deploy', roles: %w[app db web]
+server '100.27.2.43', user: 'deploy', roles: %w[app db web]
 set :rails_env, 'production'
