642a708d2e40a14c422bab8a72a062f54447cbd6
David Gasana Manzi <gmdavid59@gmail.com>
2021-08-17 18:11:40 +0200

    fix incorrect server public ip

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server '100.24.238.5', user: 'deploy', roles: %w[app db web]
+server '100.27.2.43', user: 'deploy', roles: %w[app db web]
 set :rails_env, 'production'
 
 # role-based syntax
