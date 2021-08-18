4693921ab2e7a534faa2fb1d9ffbb67cb894c478
David Gasana Manzi <gmdavid59@gmail.com>
2021-08-18 14:35:28 +0200

    resolve conflict

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
-=======
 # frozen_string_literal: true
 
->>>>>>> 98db635dffd94b3f95b0ea2e820546c1e1841d24
 class HomeController < ApplicationController
   def index
     render json: { status: 'success' }
