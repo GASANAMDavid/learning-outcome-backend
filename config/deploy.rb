b6d167824ec8319729449e421ad881b8e64ff8d2
David Gasana Manzi <gmdavid59@gmail.com>
2021-08-18 13:12:00 +0200

    fix spelling

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      execute :touch, realese_path.json('tmp/restart.txt')
+      execute :touch, release_path.json('tmp/restart.txt')
     end
   end
 
