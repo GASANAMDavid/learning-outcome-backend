b21eb386a91eb5220667e0f7588e46f932e01347
David Gasana Manzi <gmdavid59@gmail.com>
2021-08-19 07:22:14 +0200

    execute migrations

--- db/schema.rb
+++ db/schema.rb
@@ -1,8 +1,15 @@
-ActiveRecord::Schema.define(version: 20_210_808_044_708) do
+ActiveRecord::Schema.define(version: 20_210_819_051_952) do
   enable_extension 'plpgsql'
 
   create_table 'apprenticeship_levels', force: :cascade do |t|
     t.string 'title'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+  end
+
+  create_table 'roles', force: :cascade do |t|
+    t.string 'name'
+    t.string 'info'
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
   end
@@ -32,6 +39,17 @@ ActiveRecord::Schema.define(version: 20_210_808_044_708) do
     t.datetime 'updated_at', precision: 6, null: false
   end
 
+  create_table 'users', force: :cascade do |t|
+    t.string 'first_name'
+    t.string 'last_name'
+    t.string 'email'
+    t.bigint 'role_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['role_id'], name: 'index_users_on_role_id'
+  end
+
   add_foreign_key 'skills', 'apprenticeship_levels'
   add_foreign_key 'skills', 'themes'
+  add_foreign_key 'users', 'roles'
 end
