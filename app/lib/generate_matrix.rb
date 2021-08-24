551bd3e9eb1feb2d9b214def8dba6d2ac740deb5
David Gasana Manzi <gmdavid59@gmail.com>
2021-08-24 11:38:48 +0200

    refactor to archieve meaningfull name

--- app/lib/generate_matrix.rb
+++ app/lib/generate_matrix.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 class GenerateMatrix
-  def self.data(matrices)
-    matrices.map do |matrix|
+  def self.data(matrix)
+    matrix.map do |skill_record|
       {
-        id: matrix.id,
-        learning_outcome: matrix.skill.title,
-        skills_level: matrix.skills_level.id,
-        theme: { title: matrix.skill.theme.title, link: matrix.skill.theme.link },
-        apprenticeship_level: matrix.skill.apprenticeship_level.id
+        id: skill_record.id,
+        learning_outcome: skill_record.skill.title,
+        skills_level: skill_record.skills_level.id,
+        theme: { title: skill_record.skill.theme.title, link: skill_record.skill.theme.link },
+        apprenticeship_level: skill_record.skill.apprenticeship_level.id
       }
     end
   end
