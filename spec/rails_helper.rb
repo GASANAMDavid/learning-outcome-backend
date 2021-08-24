7676f19f5076b69b0807e31ebf591186ef0ec2f5
David Gasana Manzi <gmdavid59@gmail.com>
2021-08-24 07:24:29 +0200

    fix unexpected end of input

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,17 +1,11 @@
 # frozen_string_literal: true
 
-
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
-. Files matching `spec/**/*_spec.rb` are
-pattern with the --pattern
-les, manually
-sts are run.
-
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
@@ -19,16 +13,13 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
+  config.include FactoryBot::Syntax.methods
 
-  config.include FactoryBot::Syntax::methods
- 
   config.use_transactional_fixtures = true
 
   config.infer_spec_type_from_file_location!
 
-  
   config.filter_rails_from_backtrace!
-  
 end
 
 Shoulda::Matchers.configure do |config|
