cf64b53849d19ab6ee8cc5a82c460816ff2d4ea4
David Gasana Manzi <gmdavid59@gmail.com>
2021-09-08 13:44:28 +0200

    eager loading on associated tables

--- app/models/learning_outcome_matrix.rb
+++ app/models/learning_outcome_matrix.rb
@@ -8,6 +8,7 @@ class LearningOutcomeMatrix < ApplicationRecord
   has_paper_trail ignore: %i[skill_id user_id], meta: { user_id: :user_id }
 
   def self.user_matrix(user)
-    LearningOutcomeMatrix.where(user_id: user.id)
+    LearningOutcomeMatrix.includes(:user, { skill: %i[theme apprenticeship_level] },
+                                   :skills_level).where(user_id: user.id)
   end
 end
