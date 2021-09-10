2fe34027fbd0c3583062504724d7933d89d90276
David Gasana Manzi <gmdavid59@gmail.com>
2021-09-10 11:13:21 +0200

    minor fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,6 +18,14 @@ set :keep_releases, 3
 set :db_local_clean, true
 set :db_remote_clean, true
 
+namespace :rake do
+  desc 'Invoke rake task'
+  task :invoke do
+    run "cd #{deploy_to}/current"
+    run "bundle exec rake #{ENV['task']} RAILS_ENV=#{rails_env}"
+  end
+end
+
 namespace :deploy do
   desc 'Restart application'
   task :restart do
