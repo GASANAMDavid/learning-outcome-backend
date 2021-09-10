f3393d36e8399e25498a88eab5c0e31940f3cca8
David Gasana Manzi <gmdavid59@gmail.com>
2021-09-10 10:22:26 +0200

    remove rake command

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,6 @@ namespace :deploy do
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
       execute :touch, release_path.join('tmp/restart.txt')
-      execute :rake, 'setup'
     end
   end
 
