4ed1f76e0bb1bb381e93f33bea9c21a0958adf40
David Gasana Manzi <gmdavid59@gmail.com>
2021-09-15 13:33:17 +0200

    fix string literals cops errors

--- db/migrate/20210910113432_add_display_to_skills_level.rb
+++ db/migrate/20210910113432_add_display_to_skills_level.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddDisplayToSkillsLevel < ActiveRecord::Migration[6.1]
   def change
     add_column :skills_levels, :display, :string
--- db/migrate/20210915045832_learning_outcome_matrices_to_learning_outcomes.rb
+++ db/migrate/20210915045832_learning_outcome_matrices_to_learning_outcomes.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class LearningOutcomeMatricesToLearningOutcomes < ActiveRecord::Migration[6.1]
   def change
     rename_table :learning_outcome_matrices, :learning_outcomes
