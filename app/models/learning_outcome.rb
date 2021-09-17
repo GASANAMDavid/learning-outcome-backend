a1f9fcda1c2c03f670f963f7366725c32afd4fe6
David Gasana Manzi <gmdavid59@gmail.com>
2021-09-17 12:55:48 +0200

    the learning outcomes matrix monitored by papertrail

--- app/models/learning_outcome.rb
+++ app/models/learning_outcome.rb
@@ -4,6 +4,4 @@ class LearningOutcome < ApplicationRecord
   belongs_to :user
   belongs_to :skill
   belongs_to :skills_level
-
-  has_paper_trail ignore: %i[skill_id user_id], meta: { user_id: :user_id }
 end
