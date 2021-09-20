16458cb5596790171da6500b59853b8866519e6c
David Gasana Manzi <gmdavid59@gmail.com>
2021-09-20 08:56:32 +0200

    history controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@
 Rails.application.routes.draw do
   resources :learning_outcome_matrix, only: %i[index update]
   put '/learning_outcome_matrix', to: 'learning_outcome_matrix#update'
+  resources :history, only: %i[index]
 end
