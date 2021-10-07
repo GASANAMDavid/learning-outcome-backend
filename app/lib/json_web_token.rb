6f4b66ec276189dfb770a69460777393a170b715
David Gasana Manzi <gmdavid59@gmail.com>
2021-10-07 14:34:11 +0200

    use encrypted credentials for jws_keys

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -15,16 +15,15 @@ class JsonWebToken
   end
 
   def self.jwks_hash
-    jwks_raw = Net::HTTP.get URI("https://#{Rails.application.credentials.auth0[:domain]}/.well-known/jwks.json")
-    jwks_keys = Array(JSON.parse(jwks_raw)['keys'])
+    jwks_keys = Rails.application.credentials.jwks
     jwks_keys
       .map do |k|
-        [
-          k['kid'],
-          OpenSSL::X509::Certificate.new(
-            Base64.decode64(k['x5c'].first)
-          ).public_key
-        ]
-      end.to_h
+      [
+        k[:kid],
+        OpenSSL::X509::Certificate.new(
+          Base64.decode64(k[:x5c])
+        ).public_key
+      ]
+    end.to_h
   end
 end
