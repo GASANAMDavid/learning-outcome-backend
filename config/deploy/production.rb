513351d41ec6192340e08fa70fe3fc278263c704
David Gasana Manzi <gmdavid59@gmail.com>
2021-10-07 14:42:29 +0200

    change production public ip

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 
-server '100.27.2.43', user: 'deploy', roles: %w[app db web]
+server '35.173.134.155', user: 'deploy', roles: %w[app db web]
 set :rails_env, 'production'
