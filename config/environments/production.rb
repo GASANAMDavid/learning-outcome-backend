6a3676713b9c44d798245d6dba901b506d766bbe
David Gasana Manzi <gmdavid59@gmail.com>
2021-10-11 12:10:31 +0200

    safeguard in production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,4 +112,8 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  # DatabaseCleaner::Safeguard::Error::ProductionEnv:
+  DatabaseCleaner.allow_production = true
+  DatabaseCleaner.allow_remote_database_url = true
 end
