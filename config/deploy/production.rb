0ea47100b5a77bd6fc61ca9bede6e7fa9f8686a4
David Gasana Manzi <gmdavid59@gmail.com>
2021-10-11 14:01:20 +0200

    set env vars

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,3 +2,7 @@
 
 server '35.173.134.155', user: 'deploy', roles: %w[app db web]
 set :rails_env, 'production'
+set :default_environment, {
+  'DATABASE_CLEANER_ALLOW_PRODUCTION' => true,
+  'DATABASE_CLEANER_ALLOW_REMOTE_DATABASE_URL' => true
+}
