51ab6b31bce129f0b964da776b5b4691f8114554
David Gasana Manzi <gmdavid59@gmail.com>
2021-10-11 12:51:29 +0200

    change deploy task

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,18 +27,17 @@ namespace :deploy do
     end
   end
 
-  # desc 'Runs rake db:seed for SeedMigrations data'
-  # task seed: [:set_rails_env] do
-  #   on primary fetch(:migration_role) do
-  #     within release_path do
-  #       with rails_env: fetch(:rails_env) do
-  #         execute :rake, 'db:seed'
-  #       end
-  #     end
-  #   end
-  # end
+  task :seed_database do
+    on roles(:app) do
+      within release_path do
+        with rails_env: fetch(:rails_env) do
+          execute :bundle, :exec, :rake, 'db:seed'
+        end
+      end
+    end
+  end
 
-  after 'deploy:migrate', 'deploy:seed'
+  after 'deploy:migrate', 'deploy:seed_database'
   after :publishing, 'deploy:restart'
   after :finished, 'deploy:cleanup'
 end
