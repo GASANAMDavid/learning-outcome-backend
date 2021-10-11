9be184f7c91962ad2ceda182d0a6105d53e7293e
David Gasana Manzi <gmdavid59@gmail.com>
2021-10-11 11:39:06 +0200

    add database seeding task

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,13 +27,9 @@ namespace :deploy do
     end
   end
 
-  desc 'Show deployed revision'
-  task :revision do
-    on roles :app do |host|
-      within current_path do
-        info "#{host}: #{capture :cat, 'REVISION'}"
-      end
-    end
+  desc 'reload the database with seed data'
+  task :seed do
+    run "cd #{current_path}; rake db:seed RAILS_ENV=#{rails_env}"
   end
 
   after :publishing, 'deploy:restart'
