4387eeea37af22cf4fb820f4c95411877908b945
David Gasana Manzi <gmdavid59@gmail.com>
2021-10-11 14:20:57 +0200

    set up database cleaner in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
-require 'database_cleaner'
+require 'database_cleaner/active_record'
 
+DatabaseCleaner.allow_production = true
+DatabaseCleaner.allow_remote_database_url = true
 DatabaseCleaner.clean_with(:truncation)
 
 skills_levels = [
