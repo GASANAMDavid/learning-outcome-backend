40ce447b8bf7fe39e287a13355b2239aa901ca4f
David Gasana Manzi <gmdavid59@gmail.com>
2021-10-20 23:03:16 +0200

    remove default role set when create user

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,14 +1,13 @@
 class UserController < SecuredController
   skip_before_action :authorize_user, only: %i[create]
-  skip_before_action :check_is_admin?, expect: %i[list_users destroy]
+  before_action :check_is_admin?, only: %i[list_users destroy]
 
   def index
     json_response({ user: UserDetails.for(current_user) })
   end
 
   def create
-    role = Role.first
-    user = User.create!(user_params.merge(role_id: role.id))
+    user = User.create!(user_params)
     MatrixInitialization.call(user)
     json_response({ message: 'Created Successfully' }, :ok)
   end
